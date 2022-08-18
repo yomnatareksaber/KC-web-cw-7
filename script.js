4d81f8b45c68b2fef176d17187960ca475c767dc
yomnatareksaber <yomnaaaat@gmail.com>
2022-08-18 22:13:50 +0300

    classwork7

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+function BMI(weight ,height){
+    let result = weight /(height*height);
+    return result
+}
+console.log(BMI(70,1.7));
+console.log(BMI(80,1.78))
+console.log(BMI(50,1.6))
+console.log(BMI())
+console.log(BMI())
+console.log(BMI())
+console.log(BMI())
+console.log(BMI())
+
+
+
+
+function Status (bmi)
+{
+if (bmi < 18.5) {
+    return "لديك نقص في الوزن"
+}
+else if (bmi >= 18.5 && bmi <25){ return "وزنك صحي"
+}
+else{ 
+    return "لديك زيادة في الوزن "
+}
+
+}
+function calculate() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+let bmi= BMI(weight,height);
+let desc = Status(bmi);
+ let div = document.getElementById("result");
+ div.innerText= bmi +"=="+ desc;
+
+}
+
+
+
+
+
+
 
